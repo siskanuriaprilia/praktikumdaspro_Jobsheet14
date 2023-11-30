730e6556aceb9d26027b2e353715536061628000
siskanuriaprilia <aprilias878@gmail.com>
2023-11-30 17:17:08 +0700

    Percobaan227_modifikasi1

--- Rekursif_Percobaan227.java
+++ Rekursif_Percobaan227.java
@@ -2,28 +2,32 @@ import java.util.Scanner;
 
 public class Rekursif_Percobaan227 {
 
-    static int hitungPangkat(int x, int y)
+    static int hitungPangkat(int x, int y) 
     {
-        if (y == 0)
+        if (y == 0) 
         {
-            return (1);
-        }
-        else
+            System.out.print("1 ");
+            return 1;
+        } 
+        else 
         {
-            return (x * hitungPangkat(x, y - 1));
+            int hasil = x * hitungPangkat(x, y - 1);
+            System.out.print("x " + x + " ");
+            return hasil;
         }
-    } 
-    
-    public static void main(String[] args) {
-    Scanner input27 = new Scanner(System.in);
-    int bilangan, pangkat;
+    }
 
-    System.out.println("Bilangan yang dihitung: ");
-    bilangan = input27.nextInt ();
-    System.out.println("Pangkat: ");
-    pangkat = input27.nextInt ();
+    public static void main(String[] args) 
+    {
+        Scanner input27 = new Scanner(System.in);
+        int bilangan, pangkat;
 
-    System.out.println(hitungPangkat(bilangan, pangkat));
-  }
+        System.out.println("Bilangan yang dihitung: ");
+        bilangan = input27.nextInt();
+        System.out.println("Pangkat: ");
+        pangkat = input27.nextInt();
+
+        int hasilPangkat = hitungPangkat(bilangan, pangkat);
+        System.out.println("= " + hasilPangkat);
+    }
 }
-
